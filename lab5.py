839a2143e6d043f18a567355a7521c7018fdd0f6
Ajay Mandal <68213789+0xAjayMandal@users.noreply.github.com>
2022-11-04 09:56:41 +0530

    Create lab5.py

---
+++ lab5.py
@@ -0,0 +1,72 @@
+# To show the usages of various Operators available in python
+#5a
+#arithmetic operators
+a=int(input('Enter the first number:'))
+b=int(input('Enter the second number:'))
+sum=a+b
+diff=a-b
+prod=a*b
+div=a/b
+rem1=a%b
+rem2=-a%b
+rem3=a%-b
+rem4=-a%-b
+exp=a**b
+flr=a//b
+print('Sum of {} and {} is {} '.format(a,b,sum))
+print('Difference of {} and {} is {} '.format(a,b,diff))
+print('Product of {} and {} is {} '.format(a,b,prod))
+print('Quotient of {} and {} is {} '.format(a,b,div))
+print('Remainder of {} and {} is {} '.format(a,b,rem1))
+print('Remainder of -{} and {} is {} '.format(a,b,rem2))
+print('Remainder of {} and -{} is {} '.format(a,b,rem3))
+print('Remainder of -{} and -{} is {} '.format(a,b,rem4))
+print('Exponentiation of {} raised to {} is {} '.format(a,b,exp))
+print('Floor function of {} and {} is {} '.format(a,b,flr))
+
+
+#assignment operators
+x=2
+print('x=',x)
+x+=15
+print('x=',x)
+x-=5
+print('x=',x)
+x*=2
+print('x=',x)
+x/=2
+print('x=',x)
+x%=2
+print('x=',x)
+x//=2
+print('x=',x)
+x**=2
+x=10
+print('x=',x)
+x&=2
+print('x=',x)
+x|=2
+print('x=',x)
+x^=1
+print('x=',x)
+x>>=4
+print('x=',x)
+x<<=5
+print('x=',x)
+
+
+#5b
+rate=12
+overtime=0
+payoff=0
+total=0
+for i in range(0,10):
+    hours=int(input('Enter the total work hours of employee{}:'.format(i+1)))
+    if hours>40:
+        overtime=hours-40
+        payoff=overtime*rate
+        total+=payoff
+        print("Overtime payoff of employee {} is {}".format(i+1,payoff))
+    else:
+        print("Employee {} is not eligible for overtime".format(i+1))
+print("Total overtime cost:",total)
